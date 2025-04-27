2eedd96a7e481ddc5a1d35c0dfcc4099dc3b5012
jgc777 <90793213+jgc777@users.noreply.github.com>
2025-04-27 16:14:39 +0200

    fix config thing

--- HTML2EXE 2.0/BuildDialog.cs
+++ HTML2EXE 2.0/BuildDialog.cs
@@ -14,7 +14,7 @@ namespace HTML2EXE_2._0
             this.Visible = true;
             string tmpPath = Path.Combine(Path.GetTempPath(), "HTML2EXE");
             string output = Path.Combine(Environment.CurrentDirectory, "out.exe");
-            string configTitle = JsonNode.Parse(File.ReadAllText(Path.Combine(tmpPath, "config.json")))["title"].ToString();
+            string configTitle = JsonNode.Parse(File.ReadAllText(Path.Combine(tmpPath, "config.json")))["title"]?.ToString() ?? null;
             if (configTitle != null) output = Path.Combine(Environment.CurrentDirectory, configTitle + ".exe");
             Program.build(output);
         }
